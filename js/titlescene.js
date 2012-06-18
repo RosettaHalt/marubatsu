33a7a5e831ca15e4386ef951a12fa331b9b36399
RosettaHalt <rosetta@lugn-design.com>
2012-06-18 22:31:40 +0900

    Changed title.

--- js/titlescene.js
+++ js/titlescene.js
@@ -9,7 +9,7 @@
             var label = tm.app.Label(32,32);
             label.x = app.width/3;
             label.y = app.height/2;
-            label.text = "Speed Calculation";
+            label.text = "Formula Front";
             label.text.align = "end";
             label.width = app.width
             this.addChild(label);
